1dc45b25d03f04de39296b3a6bd4316b24e66902
woravich-k <woravich@hotmail.com>
2018-05-08 21:52:25 -0400

    Add comment

--- uczlumt/www/js/main.js
+++ uczlumt/www/js/main.js
@@ -1,15 +1,22 @@
-var geoJSONData
+var geoJSONData;
+var geoJSON; //keep it as Global variable, so doesn't have to be loaded often.
 var client;
 
+//								********************** Functions to create list of the questions table **************************
+
+//firstly load data from the DB  
+//adapted from the practicle codes in the module
 function loadQuestion(){
+	//requesting geoJSON of the questions to create the list of the question
 	var url = 'https://developer.cege.ucl.ac.uk:31083/getGeoJSON/question/geom'
 	client = new XMLHttpRequest();
 	
 	client.open('GET', url);
-	client.onreadystatechange = dataResponse; //note don't use earthquakeResponse() with brackets as that doesn't work
+	client.onreadystatechange = dataResponse; 
 	client.send();
 }
 
+//adapted from the practicle codes in the module
 //create the code to wait for the response from the data server, and process the response once it is received
 function dataResponse(){
 //this function listens out for the server to say that the data is ready - i.e. has state 4
@@ -19,48 +26,50 @@ function dataResponse(){
 		listQuestion(geoJSONData);
 	}
 }
-var geoJSON
+
+// after getting a JSON, create a table by changin the table DIV (Question list)
 function listQuestion(geoJSONData){
 	geoJSON = JSON.parse(geoJSONData);
 	var htmlString = "<table> <thead> <tr><th>#</th><th>Question</th><th>Answer</th><th style = 'width: 44.4px'>Edit</th><th style = 'width: 44.4px'>Delete</th> </tr></thead><tbody>";
+	
+	//for loop to create HTML code regard to the GeoJSON data that recieve from the server
 	for(var i = 0; i < geoJSON[0].features.length; i++) {
 		var feature = geoJSON[0].features[i];
 		htmlString = htmlString + "<tr> <td>" + (i+1) + "</td>"
 		htmlString = htmlString + "<td>" + feature["properties"]["question"] + "</td>"
 		htmlString = htmlString + "<td>" + feature["properties"][feature["properties"]["answer"]] + "</td>"
-		htmlString = htmlString + "<td> <button id = 'edit" + feature["properties"]["id"] + "' class = 'btn' onclick ='editQuestion(this.name)' name = '" + i + "'> <i class = 'material-icons'>edit</i></button></td>"
+		//edit button
+		//set their name as an index of the GeoJSON and pass to the function by this.name, to access other attributes to the selected row without looping again
+		htmlString = htmlString + "<td> <button id = 'edit" + feature["properties"]["id"] + "' class = 'btn' onclick ='editQuestion(this.name)' name = '" + i + "'> <i class = 'material-icons'>edit</i></button></td>" 
+		
+		//delete button
+		//set their name as an id(primary key of the question table) of the record, pass to delete function by this.name.
+		//This is different from the edit since other attributes are not needed 
 		htmlString = htmlString + "<td> <button id = 'delete" + feature["properties"]["id"] + "' class = 'btn' onclick ='deleteQuestion(this.name)' name = '"+ + feature["properties"]["id"]+ "'> <i class = 'material-icons'>delete</i></button></td>"
 		
 		htmlString = htmlString + "</tr>"
 		
 	}
 	htmlString = htmlString + "</tbody></table>"
+	
+	//change DIV
 	document.getElementById("Question list").innerHTML = htmlString
 }
 
 
-function deleteQuestion(id){
-	if (confirm("Press OK to delete this question?") == false) return;
-	deleteURL = "https://developer.cege.ucl.ac.uk:31083/deleteRow/question/" + id;
-	client = new XMLHttpRequest();
-	client.open('GET', deleteURL);
-	client.onreadystatechange = deleteResponse; 
-	client.send();
-}
 
-function deleteResponse(){
-	//this function listens out for the server to say that the data is ready - i.e. state 4
-	if(client.readyState == 4){
-		//change the DIV show the response
-		alert(client.responseText);
-		loadQuestion();
-	}
-}
+
+
+
+//										********************** Functions to load question form for either adding new or editing question**************************
 
 var xhr; //define the globle to process the AJAX request
 
+//loading question form via AJAX (four_choices.html)
+//using AJAX to open up opotunity to have other types of question apart from four choices - i.e. type in question, true-false question, multiple correct answers question
 
-function addNew(){
+//adapted from the practicle codes in the module
+function addNew(){ //load if user want to add new function
 	var url = "./four_choices.html";
 	xhr = new XMLHttpRequest();
 	//var url = document.getElementById("url").value;
@@ -79,7 +88,7 @@ function addNew(){
 	xhr.send();
 	
 }
-
+//adapted from the practicle codes in the module
 function processDivChange_addNew(){
 	//while waiting response from server
 	if(xhr.readyState <4)  document.getElementById('question form').innerHTML="Loading...";
@@ -87,16 +96,19 @@ function processDivChange_addNew(){
 	else if (xhr.readyState === 4){
 		//200-300 --> all successful
 		if (xhr.status==200&&xhr.status<300) {
-			document.getElementById('question form').innerHTML=xhr.responseText;
-			loadmap();
+			document.getElementById('question form').innerHTML=xhr.responseText; //wait for the respose then change the DIV(question form)
+			loadmap(); //load map after the div is loaded
 		}
 	}
 }
 
-var edit_id;
-function editQuestion(id){
+
+//functions for loading form for editing
+//adapted from the practicle codes in the module
+var edit_id; //global variable to use after getting response
+function editQuestion(id){ //get id from this.name
 	edit_id = id;
-	var url = "./four_choices.html";
+	var url = "./four_choices.html"; //load from the same html as adding new to get the basic structure
 	xhr = new XMLHttpRequest();
 	//var url = document.getElementById("url").value;
 	if (url == ""){
@@ -114,15 +126,19 @@ function editQuestion(id){
 	xhr.send();
 }
 
+//adapted from the practicle codes in the module
 function processDivChange_edit(){
 	//while waiting response from server
 	if(xhr.readyState <4)  document.getElementById('question form').innerHTML="Loading...";
 	///4 = response from server has been completely loaded.
 	else if (xhr.readyState === 4){
 		//200-300 --> all successful
-		if (xhr.status==200&&xhr.status<300) {
+		if (xhr.status==200&&xhr.status<300) { //after the html is loaded, it is still not suit with editing question, so adapt some part
 			document.getElementById('question form').innerHTML=xhr.responseText;
-			document.getElementById("head").innerHTML = "Edit question: #" + (Number(edit_id)+1);
+			//change header
+			document.getElementById("head").innerHTML = "Edit question: #" + (Number(edit_id)+1); 
+			
+			//fill in the form by the information before editing
 			document.getElementById("question").value = geoJSON[0].features[edit_id]["properties"]["question"];
 			document.getElementById("qurl").value = geoJSON[0].features[edit_id]["properties"]["qurl"];
 			document.getElementById("choice1").value = geoJSON[0].features[edit_id]["properties"]["choice1"];
@@ -139,12 +155,21 @@ function processDivChange_edit(){
 			var correctANS = geoJSON[0].features[edit_id]["properties"]["answer"];
 			var ans = "ans" + correctANS.charAt(correctANS.length-1);
 			document.getElementById(ans).checked = true;
+			
+			//change the button from create new question to edit question
 			document.getElementById("upload").innerHTML = "<button id = 'startUpload' class = 'btn' onclick ='startDataUpdate("+ geoJSON[0].features[edit_id]["properties"]["id"] +")'> <i class = 'material-icons'>update</i>  Edit Question</button>";
-			loadmap();
-			edit_id = null;
-			loadmap();
+			
+			loadmap(); //load map after the div is loaded
+			edit_id = null; //reset global variable
+			
 		}
 	}
 }
 
+//cancel function to remove the form without adding or editing
+function cancel(){
+	document.getElementById('question form').innerHTML = ""
+}
+
+//list the question at the first time that the site is loaded
 loadQuestion();
--- uczlumt/www/js/uploadData.js
+++ uczlumt/www/js/uploadData.js
@@ -1,15 +1,18 @@
 
 //							****** load simple map and define some icons ******
-//load the map
+
 var lat;
 var lon;
 var mymap;
 var myMarker;
+//define as function to load leaflet map whenever the question form is loaded
+//adapted from the practicle codes in the module
 function loadmap(){
+	//read lat and lon in the coordinates boxes to use as marker and center of map
 	lat = document.getElementById("lat").value;
 	lon = document.getElementById("lon").value;
 	mymap = L.map('mapid').setView([lat,lon],13);
-		
+	//load the map	
 	//load the tiles
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',{
 		maxZoom: 18,
@@ -30,6 +33,10 @@ function loadmap(){
 		});
 
 }
+
+
+//		********** Functions for maker and input box *********** 
+//move maker on change of the input boxes 
 function movemarker(){
 	lat = document.getElementById("lat").value;
 	lon = document.getElementById("lon").value;
@@ -38,6 +45,7 @@ function movemarker(){
 	mymap.fitBounds(myMarker.getLatLng().toBounds(3000));
 }
 
+//function for locate me button
 function locateMe(){
 	navigator.geolocation.getCurrentPosition(function(position){
 		document.getElementById("lat").value = position.coords.latitude;
@@ -46,14 +54,25 @@ function locateMe(){
 	});
 }
 
+
+
+
+
+
+//		*************************** Functions for manipulating questions **********************
+
+//		********** Functions for creating new question *********** 
+//adapted from the practicle codes in the module
 //function for upload data from the form
 function startDataUpload(){
+	//read inputs
 	var question = document.getElementById("question").value;
 	var qurl = document.getElementById("qurl").value;
 	var choice1 = document.getElementById("choice1").value;
 	var choice2 = document.getElementById("choice2").value;
 	var choice3 = document.getElementById("choice3").value;
 	var choice4 = document.getElementById("choice4").value;
+	//check if the form is not filled completely alert to the user
 	if (choice1.trim() == '' || choice2.trim() == ''|| choice3.trim() == ''|| choice4.trim() == ''||question.trim() == ''){
 		alert('Question and Choices have to be specified!!!');
 		return;
@@ -63,7 +82,7 @@ function startDataUpload(){
 	var choice3url = document.getElementById("choice3url").value;
 	var choice4url = document.getElementById("choice4url").value;
 	
-	
+	//crete post string
 	var postString = "question="+question +"&qurl="+qurl + "&choice1="+choice1 + "&choice1url="+choice1url + "&choice2="+choice2 + "&choice2url="+choice2url + "&choice3="+choice3 + "&choice3url="+choice3url+ "&choice4="+choice4 + "&choice4url="+choice4url +"&latitude="+lat +"&longitude="+lon;
 	
 	var answer;
@@ -75,10 +94,11 @@ function startDataUpload(){
 		answer='choice3';
 	}else if(document.getElementById("ans4").checked){
 		answer='choice4';
-	} else {
+	} else { //check if the form is not filled completely alert to the user
 		alert('Please select the correct answer!!!');
 		return
 	}
+	//answer will store as choice, choice2 ... but fullanswer will get the value of that choice
 	postString = postString + "&answer=" +answer;
 	var fullanswer = document.getElementById(answer).value;
 	postString = postString + "&fullanswer="+fullanswer;
@@ -86,22 +106,29 @@ function startDataUpload(){
 	postData(postString);
 }
 
+//adapted from the practicle codes in the module
+//post data to the webserver 
 var client;
 function postData(postString){
 	client = new XMLHttpRequest();
 	client.open('POST','https://developer.cege.ucl.ac.uk:31083/uploadQuestion',true);
 	client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	//wait for the respond and condunct dataUploaded function
 	client.onreadystatechange = dataUploaded;
 	client.send(postString);
 }
 
-//create hte code to wait for the response from the data server, and process the response once it is received
+//adapted from the practicles of the module
+//create the code to wait for the response from the data server, and process the response once it is received
+//report/alert the reponse
+//this is shared function with editing
 function dataUploaded(){
 	//this function listens out for the server to say that the data is ready - i.e. state 4
 	if(client.readyState == 4){
-		//change the DIV show the response
-		if (client.status == 400){
+		
+		if (client.status == 400){ //400 is error according to the server side code
 			if (client.responseText == "question_geom_unique"){
+				// if the question is already taken by another question alert to the user
 				document.getElementById("lat").style.border = "2px solid red";
 				document.getElementById("lon").style.border = "2px solid red";
 				alert("This location is already taken by the another question.");
@@ -112,14 +139,14 @@ function dataUploaded(){
 		}
 		alert(client.responseText);
 		document.getElementById('question form').innerHTML = ""
-		loadQuestion();
+		loadQuestion(); //reload the question list after adding or editing
 	}
 }
 
-function cancel(){
-	document.getElementById('question form').innerHTML = ""
-}
 
+//		********** Functions for editing exiting question *********** 
+//adapted from the practicle codes in the module
+//id is sent by the button
 function startDataUpdate(id){
 	// alert(id);
 	var question = document.getElementById("question").value;
@@ -128,6 +155,8 @@ function startDataUpdate(id){
 	var choice2 = document.getElementById("choice2").value;
 	var choice3 = document.getElementById("choice3").value;
 	var choice4 = document.getElementById("choice4").value;
+	
+	//check if the form is not filled completely alret to the user
 	if (choice1.trim() == '' || choice2.trim() == ''|| choice3.trim() == ''|| choice4.trim() == ''||question.trim() == ''){
 		alert('Question and Choices have to be specified!!!');
 		return;
@@ -150,22 +179,48 @@ function startDataUpdate(id){
 	}else if(document.getElementById("ans4").checked){
 		answer='choice4';
 	} else {
-		alert('Please select the correct answer!!!');
+		alert('Please select the correct answer!!!'); //check if the form is not filled completely alret to the user
 		return
 	}
+	//answer will store as choice, choice2 ... but fullanswer will get the value of that choice
 	postString = postString + "&answer=" +answer;
 	var fullanswer = document.getElementById(answer).value;
 	postString = postString + "&fullanswer="+fullanswer;
+	
+	//post similar parameters as create new question, plus question id
 	postString = postString +"&id=" + id;
 	//alert(postString);
 	processDataEdit(postString);
 }
-
+//post to the server
 function processDataEdit(postString){
 	client = new XMLHttpRequest();
 	client.open('POST','https://developer.cege.ucl.ac.uk:31083/editQuestion/',true);
 	client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	//use shared function to wait the respond with create new question
 	client.onreadystatechange = dataUploaded;
 	client.send(postString);
+}
+
+
+//		********** Functions for deleting exiting question ***********
+//using GET insead of post since there are only few parameters, (id and table name)
+//get id from the buttons
+function deleteQuestion(id){
+	if (confirm("Press OK to delete this question?") == false) return;
+	deleteURL = "https://developer.cege.ucl.ac.uk:31083/deleteRow/question/" + id;
+	client = new XMLHttpRequest();
+	client.open('GET', deleteURL);
+	client.onreadystatechange = deleteResponse; 
+	client.send();
+}
+
+function deleteResponse(){
+	//this function listens out for the server to say that the data is ready - i.e. state 4
+	if(client.readyState == 4){
+		//change the DIV show the response
+		alert(client.responseText);
+		loadQuestion(); //reload the question list after deleting
+	}
 }
 
